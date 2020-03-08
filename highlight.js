a2ecbbcae10b40202a82337c6d879bb17b1294a5
Anna Qin <anna.qin418@gmail.com>
2020-03-08 14:29:48 -0400

    slight animation mod

--- highlight.js
+++ highlight.js
@@ -3,8 +3,8 @@ var links = document.getElementsByTagName('a');
 // var links = document.getElementById('links');
 //  for(var i = 0,length=links.length>10 ? 10 : links.length; i < length; i++) links[i].style.backgroundColor = '#F00';
 for (let i = 0; i < links.length; i++) {
-    links[i].style.backgroundColor = '#EE0';
-    links[i].style.borderLeft = '25px solid #000'
+    // links[i].style.backgroundColor = '#EE0';
+    links[i].style.borderLeft = '20px solid #000'
     links[i].animate([
         // keyframes
         { borderLeftColor: '#000' },
@@ -14,6 +14,7 @@ for (let i = 0; i < links.length; i++) {
         // timing
         delay: i % 10 * 500,
         duration: 3000,
-        iterations: Infinity
+        iterations: Infinity,
+        easing: 'ease-in'
     })
 }
